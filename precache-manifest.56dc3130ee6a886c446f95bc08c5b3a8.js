dd5c1ca7a96f7fcaf7f7f1ceec2d7ebd8f5f0da5
Ivan Sharov <asddsa356@mail.ru>
2018-11-18 16:11:35 +0300

    Add files via upload

---
+++ precache-manifest.56dc3130ee6a886c446f95bc08c5b3a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab19465fd187e80813e5",
+    "url": "/github.io/bot/static/js/runtime~main.ab19465f.js"
+  },
+  {
+    "revision": "a2d45b9f62bb223e6dca",
+    "url": "/github.io/bot/static/js/main.a2d45b9f.chunk.js"
+  },
+  {
+    "revision": "c1cc9d5149c71ed9c59f",
+    "url": "/github.io/bot/static/js/1.c1cc9d51.chunk.js"
+  },
+  {
+    "revision": "a2d45b9f62bb223e6dca",
+    "url": "/github.io/bot/static/css/main.6b687071.chunk.css"
+  },
+  {
+    "revision": "43218ecd75b395d23a568b963272e951",
+    "url": "/github.io/bot/index.html"
+  }
+];
